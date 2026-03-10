05b752dc6346d5cf66b235c34bd7b07cd19b75e8
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2026-03-11 02:46:22 +0800

    feat: add player basic interaction with fragments

--- Player/Player.cs
+++ Player/Player.cs
@@ -31,10 +31,15 @@ public partial class Player : CharacterBody2D
 	[Export] public Vector2 JumpStretchScale = new(0.92f, 1.1f);
 	[Export] public Vector2 LandingSquashScale = new(1.12f, 0.88f);
 	[Export] public float LandingSquashVelocityRange = 900.0f;
-
+	[Export] public float FragmentXAmplitude = 900f;
+	[Export] public float FragmentYAmplitude = 0.07f;
+	[Export] public float FragmentHoldLerpSpeed = 0.5f;
+	[Export] public float FragmentDefaultHoldDistance = 50f;
 	public Node2D Visual => field ??= GetNode<Node2D>("Visual");
+	public Area2D PickupArea => field ??= GetNode<Area2D>("%PickupArea");
 	public Vector2 BaseVisualScale { get; set; } = Vector2.One;
 	public Vector2 ImpactVisualScale { get; set; } = Vector2.One;
+	public Fragment HeldFragment { get; set; } = null;
 	public float MoveInput { get; set; } = 0.0f;
 	public int FacingDirection { get; set; } = 1;
 	public float VisualTime { get; set; } = 0.0f;
@@ -42,23 +47,28 @@ public partial class Player : CharacterBody2D
 	public float JumpBufferTimer { get; set; } = 0.0f;
 	public float LandingImpactSpeed { get; set; } = 0.0f;
 	public float CutSceneTargetXPos { get; set; } = 0.0f;
-	public async void CutSceneMove(float xPos)
+	public ulong CutSceneMoveRequestId { get; set; } = 0;
+
+	private async void DetectCutSceneArrival(float xPos, ulong requestId)
+	{
+		while (requestId == CutSceneMoveRequestId)
+		{
+			bool arrived = Mathf.Abs(GlobalPosition.X - xPos) <= 1.0f;
+			if (arrived)
+			{
+				CutSceneMoveDir = 0f;
+				break;
+			}
+
+			await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
+		}
+	}
+
+	public void CutSceneMove(float xPos)
 	{
 		CutSceneTargetXPos = xPos;
-		Action detectArrival = async () =>
-		{
-			while (true)
-			{
-				bool arrived = Mathf.Abs(GlobalPosition.X - xPos) <= 1.0f;
-				if (arrived)
-				{
-					CutSceneMoveDir = 0f;
-					break;
-				}
-				await ToSignal(GetTree(), SceneTree.SignalName.PhysicsFrame);
-			}
-		};
-		detectArrival?.Invoke();
+		CutSceneMoveRequestId++;
 		CutSceneMoveDir = Mathf.Sign(xPos - GlobalPosition.X);
+		DetectCutSceneArrival(xPos, CutSceneMoveRequestId);
 	}
 }
