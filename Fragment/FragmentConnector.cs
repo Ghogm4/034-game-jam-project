db74056a8d49e5d3222959fbb58af1a7247596ed
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2026-03-14 15:16:56 +0800

    fix: solve merge conflicts

--- Fragment/FragmentConnector.cs
+++ Fragment/FragmentConnector.cs
@@ -3,27 +3,6 @@ using System;
 
 public partial class FragmentConnector : Node
 {
-<<<<<<< HEAD
-	[Export] public float LineWidth = 4.0f;
-	[Export] public float MinAlpha = 0.2f;
-	[Export] public float MaxAlpha = 0.5f;
-	[Export] public float BlinkDuration = 0.5f;
-	private bool _isDirty = true;
-	private Godot.Collections.Array<Fragment> Fragments
-	{
-		get
-		{
-			if (_isDirty)
-			{
-				GD.Print("Refreshing fragment list in FragmentConnector...");
-				field = new Godot.Collections.Array<Fragment>();
-				foreach (var node in GetTree().GetNodesInGroup("Fragments"))
-				{
-					if (node is Fragment fragment)
-					{
-						field.Add(fragment);
-						if (fragment.IsConnected(Fragment.SignalName.Collected, Callable.From<Fragment>(Connect))) continue;
-=======
     [Export] public float LineWidth = 4.0f;
     [Export] public float MinAlpha = 0.2f;
     [Export] public float MaxAlpha = 0.5f;
@@ -49,7 +28,6 @@ public partial class FragmentConnector : Node
                     {
                         field.Add(fragment);
                         if (fragment.IsConnected(Fragment.SignalName.Collected, Callable.From<Fragment>(Connect))) continue;
->>>>>>> e44dafe8f8fd091b6ae4d56f6708c4da28c83671
 
 						fragment.Connect(Fragment.SignalName.Collected, Callable.From<Fragment>(Connect));
 						fragment.Connect(Fragment.SignalName.TreeExiting, Callable.From(OnFragmentTreeExiting));
@@ -73,71 +51,6 @@ public partial class FragmentConnector : Node
 			_isDirty = true;
 	}
 
-<<<<<<< HEAD
-	private void RefreshFragments()
-	{
-		var _ = Fragments;
-	}
-	private void Connect(Fragment centerFragment)
-	{
-		GD.Print($"Connecting from fragment: {centerFragment.Name}");
-		foreach (var frag in Fragments)
-		{
-			if (frag is not Fragment fragment) continue;
-			if (fragment != centerFragment)
-			{
-				GD.Print($"Connecting to fragment: {fragment.Name}");
-				ConnectFragments(centerFragment, fragment);
-			}
-		}
-	}
-	private async void UpdateLine(Line2D line, Fragment fragA, Fragment fragB)
-	{
-		while (IsInstanceValid(line) && IsInstanceValid(fragA) && IsInstanceValid(fragB))
-		{
-			line.SetPointPosition(0, fragA.GlobalPosition);
-			line.SetPointPosition(1, fragB.GlobalPosition);
-			await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
-		}
-		if (IsInstanceValid(line) && (line?.IsInsideTree() ?? false))
-		{
-			line.QueueFree();
-		}
-	}
-	private void ConnectFragments(Fragment fragA, Fragment fragB)
-	{
-		Line2D line = new Line2D()
-		{
-			DefaultColor = new Color(1, 1, 1, MinAlpha),
-			Width = LineWidth
-		};
-		CallDeferred(MethodName.AddChild, line);
-		line.Points = [fragA.GlobalPosition, fragB.GlobalPosition];
-		UpdateLine(line, fragA, fragB);
-		MakeLineBlink(line);
-	}
-	private void MakeLineBlink(Line2D line)
-	{
-		Tween tween = line.CreateTween();
-		tween.TweenProperty(line, "default_color:a", MaxAlpha, BlinkDuration)
-			.SetEase(Tween.EaseType.InOut)
-			.SetTrans(Tween.TransitionType.Sine);
-		tween.TweenProperty(line, "default_color:a", MinAlpha, BlinkDuration)
-			.SetEase(Tween.EaseType.InOut)
-			.SetTrans(Tween.TransitionType.Sine);
-		tween.SetLoops();
-	}
-	private void OnFragmentTreeExiting()
-	{
-		_isDirty = true;
-		CallDeferred(MethodName.RefreshFragments);
-	}
-	private void Disconnect()
-	{
-		foreach (var child in GetChildren())
-		{
-			if (child is not Line2D line) continue;
-=======
     private void RefreshFragments()
     {
         var _ = Fragments;
@@ -201,7 +114,6 @@ public partial class FragmentConnector : Node
         foreach (var child in GetChildren())
         {
             if (child is not Line2D line) continue;
->>>>>>> e44dafe8f8fd091b6ae4d56f6708c4da28c83671
 
 			line.QueueFree();
 		}
